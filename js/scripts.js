59a1cfbc9f51655b2fc53ff09c2a3b5809740935
Dennis Kiboi <mururi.kiboi@gmail.com>
2023-03-30 16:59:28 +0300

    Change article cards to 4 columns

--- js/scripts.js
+++ js/scripts.js
@@ -14,21 +14,26 @@ document.addEventListener("DOMContentLoaded", () => {
   let baseURL = "https://newsapi.org";
   let apiKey = "a6702f9745414dbdb2031e3fd6930800";
  
-  let articleTitle = document.querySelector(".card-title");
-  let author = document.querySelector('h6');
-  let Description = document.querySelector("h7")
-  let source = document.querySelector("h8")
- let published = document.querySelector("h9")
+  let row = document.querySelector('.row');
 
   fetch(baseURL + '/v2/top-headlines?category=technology&language=en&apiKey=' + apiKey)
     .then(response => response.json())
     .then(data => {
-      console.log(data.articles[0]);
-      articleTitle.textContent = data.articles[0].title;
-      author.textContent = data.articles[0].author;
+      const articles = data.articles;
+      console.log(articles); // An array of articles
+
+      articles.forEach(article => {
+        let articleCard = document.createElement('div');
+        articleCard.classList.add("col-md-3");
+        articleCard.innerHTML = ` <div class="card mb-4" style="width: 18rem;">
+                                    <img src=${article.urlToImage} class="card-img-top" alt="">
+                                    <div class="card-body">
+                                      <h5 class="card-title">${article.title}</h5>
+                                      <h6>Author</h6>
+                                      <p class="card-text">Discover the top 5 Mi phones with high-quality cameras for breathtaking</p>
+                                    </div>
+                                  </div>`
+        row.appendChild(articleCard);
+      });
     });
-
-  
-   
 });
-
