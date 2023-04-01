c41eb9407dd083efcfc78c519fbc217ac3eaf7e7
tugi <blueqwinn@gmail.com>
2023-04-01 14:57:19 +0300

    add author details

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
                                     <img src=${article.urlToImage} class="card-img-top" alt="">
                                     <div class="card-body">
                                       <h5 class="card-title">${article.title}</h5>
-                                      <h6>Author</h6>
-                                      <p class="card-text">Discover the top 5 Mi phones with high-quality cameras for breathtaking</p>
+                                      <h6>${article.author}</h6>
                                     </div>
                                   </div>`
         row.appendChild(articleCard);
