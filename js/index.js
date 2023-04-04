d17c19be3222ff67dd41e1dcc58d9e0f94b78225
tugi <blueqwinn@gmail.com>
2023-04-04 16:46:51 +0300

    add localhtml and js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
+// let cards = document.querySelectorAll('.card');
+// console.log(cards);
+
+// Get single elements from the DOM
+// document.querySelector('any-css-selector')
+// document.getElementById('id-name')
+
+//Get multiple elements from the DOM
+// document.querySelectorAll('any-css-selector')
+// document.getElementsByClassName('class-name')
+// document.getElementsByTagName('tag-name')
+
 document.addEventListener("DOMContentLoaded", () => {
-    let baseURL = " https://newsapi.org/v2/top-headlines";
+    let baseURL = "https://newsapi.org";
     let apiKey = "a6702f9745414dbdb2031e3fd6930800";
    
-    let row = document.querySelector('.row');
+    let localNewsContent = document.querySelector('#recent-news-content');
   
     fetch(baseURL + '/v2/top-headlines?category=technology&language=en&apiKey=' + apiKey)
       .then(response => response.json())
@@ -17,11 +29,11 @@ document.addEventListener("DOMContentLoaded", () => {
                                       <img src=${article.urlToImage} class="card-img-top" alt="">
                                       <div class="card-body">
                                         <h5 class="card-title">${article.title}</h5>
-                                        <h6>${article.author}</h6>
-              
+                                        <h6>Author</h6>
+                                        <p class="card-text">Discover the top 5 Mi phones with high-quality cameras for breathtaking</p>
                                       </div>
                                     </div>`
-          row.appendChild(articleCard);
-           });
+          localNewsContent.appendChild(articleCard);
+        });
       });
   });
